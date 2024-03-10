366fc4959c4b74863267a54d845ee08fd4969530
0balam0 <dmr.matteo@gmail.com>
2024-03-11 00:24:42 +0100

    risolti alcuni bug

--- myScript.js
+++ myScript.js
@@ -100,145 +100,12 @@ function showSlides(n) {
   dots[slideIndex-1].className += " active";
 //  captionText.innerHTML = dots[slideIndex-1].alt;
 }
-//
-function caricaFotoDaFile_small(fileInput){
-    var imgNumber = 1;
-    fetch(fileInput)
-      .then(response => response.text())
-      .then(data => {
-        const lines = data.split('\n');
-        const container = document.getElementById('galleriaSmall');
-        lines.forEach(line => {
-          const div = document.createElement('div');
-          div.classList.add("galleriaScacchiera_photoDiv");
-          div.id="placeHere";
-          var elem = document.createElement("img");
-          elem.setAttribute("src", line+256);
-          elem.setAttribute("style", "width:100%");
-          elem.setAttribute("onclick", "openModal();currentSlide("+imgNumber+")");
-          elem.setAttribute("class", "hover-shadow cursor")
-          container.appendChild(div);
-          document.getElementById("placeHere").appendChild(elem);
-          document.getElementById("placeHere").id="";
-          imgNumber=imgNumber+1;
-        });
-      })
-//      .catch(error => console.error(error));
-}
-function caricaFotoDaFile_big(fileInput){
-var imgNumber = 1;
-    fetch(fileInput)
-      .then(response => response.text())
-      .then(data => {
-        const lines = data.split('\n');
-        const container = document.getElementById('galleriaBig');
-        lines.forEach(line => {
-//        img grandi
-          const div = document.createElement('div');
-          div.classList.add("mySlides");
-          div.id="placeHere";
-          var elem = document.createElement("img");
-          elem.setAttribute("src", line+1024);
-          elem.setAttribute("style", "width:100%");
-          container.appendChild(div);
-          document.getElementById("placeHere").appendChild(elem);
-          document.getElementById("placeHere").id="";
-        });
-//        lines.forEach(line => {
-//        //          img piccole (miniature)
-//          const divM = document.createElement('div');
-//          divM.classList.add("column");
-//          divM.id="placeHere";
-//          var elem = document.createElement("img");
-//          elem.classList.add("demo");
-//          elem.classList.add("cursor");
-//          elem.setAttribute("src", line+256);
-//          elem.setAttribute("onclick","currentSlide("+imgNumber+")");
-//          elem.setAttribute("alt","Nature and sunrise");
-//          elem.setAttribute("style", "width:100%");
-//          container.appendChild(divM);
-//          document.getElementById("placeHere").appendChild(elem);
-//          document.getElementById("placeHere").id="";
-//          imgNumber=imgNumber+1;
-//      })
-      });
-//      .catch(error => console.error(error));
-}
-//----google Photo load
-function listGoogleFolderFile(){
-    const apiKey = "AIzaSyB9sIIxnF3LTS6-7oN-D3HbV4ZGOz5ReT4";
-    const folderId = '1MRnPPdsmNHNHdtSG0sSpuKkNmdrmUQiC';
-//    https://drive.google.com/drive/folders/1MRnPPdsmNHNHdtSG0sSpuKkNmdrmUQiC?usp=sharing
-//GET https://www.googleapis.com/drive/v2/files?q=a1MRnPPdsmNHNHdtSG0sSpuKkNmdrmUQiC&key=AIzaSyB9sIIxnF3LTS6-7oN-D3HbV4ZGOz5ReT4
-    const apiUrl = `https://www.googleapis.com/drive/v3/files?q='${folderId}' in parents&key=${apiKey}`;
-    console.log(apiUrl);
-//    console.log(apiUrl);
-    fetch(apiUrl)
-    .then(response => response.json())
-    .then(data => {
-        const files = data.files;
-        if (files && files.length > 0) {
-          files.forEach(file => {
-            console.log(file.name, '(', file.id, ')');
-          });
-        } else {
-          console.log('No files found.');
-        }
-        })
-        .catch(error => {
-        console.error('Error:', error);
-    });
-}
-function caricaFotoDaGoogle_small(folderId){
-var imgNumber = 1;
-    const apiKey = "AIzaSyB9sIIxnF3LTS6-7oN-D3HbV4ZGOz5ReT4";
-    const apiUrl = `https://www.googleapis.com/drive/v3/files?q='${folderId}' in parents&key=${apiKey}`;
-    console.log(apiUrl);
-    fetch(apiUrl)
-    .then(response => response.json())
-    .then(data => {
-        const container = document.getElementById('galleriaSmall');
-        const containerBig = document.getElementById('galleriaBig');
-        const files = data.files;
-        if (files && files.length > 0) {
-            files.forEach(file => {
-//            costruisco le img piccole
-                const div = document.createElement('div');
-                div.classList.add("galleriaScacchiera_photoDiv");
-                div.id="placeHere";
-                var elem = document.createElement("img");
-//                https://www.googleapis.com/drive/v3/files/1c9biQJcw4hbG19iV318EPLet53LPSMSt?alt=media&key=AIzaSyB9sIIxnF3LTS6-7oN-D3HbV4ZGOz5ReT4
-                elem.setAttribute("src", "https://www.googleapis.com/drive/v3/files/"+file.id+'?alt=media&key='+apiKey);
 
-//                elem.setAttribute("src", "https://drive.google.com/uc?export=view&id="+file.id);
-                elem.setAttribute("alt","Immagine da Google Drive");
-//                                elem.setAttribute("src", "https://drive.usercontent.google.com/download?export=view&authuser=0&id=1hEBbe4Jb6MbaDokcmCLorSpwvh66Xen3");
+//######################################################################################################################
+// TOGLIERE IL COMMENTO DA QUA
+//######################################################################################################################
 
-                elem.setAttribute("style", "width:100%; max-height: 300px; ");
-                elem.setAttribute("onclick", "openModal();currentSlide("+imgNumber+")");
-                elem.setAttribute("class", "hover-shadow cursor")
-                container.appendChild(div);
-                document.getElementById("placeHere").appendChild(elem);
-                document.getElementById("placeHere").id="";
-                imgNumber=imgNumber+1;
- //            costruisco le img grandi
-                const divBig = document.createElement('div');
-                divBig.classList.add("mySlides");
-                divBig.id="placeHere";
-                var elemBig = document.createElement("img");
-                 elemBig.setAttribute("src", "https://www.googleapis.com/drive/v3/files/"+file.id+'?alt=media&key='+apiKey);
-//                elemBig.setAttribute("src", "https://drive.google.com/uc?export=view&id="+file.id);
-                elemBig.setAttribute("alt","Immagine da Google Drive");
-//                                elemBig.setAttribute("src", "https://drive.usercontent.google.com/download?export=view&authuser=0&id=1hEBbe4Jb6MbaDokcmCLorSpwvh66Xen3");
-                elemBig.setAttribute("style", "width:100%;");
-                containerBig.appendChild(divBig);
-                document.getElementById("placeHere").appendChild(elemBig);
-                document.getElementById("placeHere").id="";
-            });
-        }
-    });
-}
-
+//function caricaFotoDaFile_small(fileInput){
 //    var imgNumber = 1;
 //    fetch(fileInput)
 //      .then(response => response.text())
@@ -262,3 +129,140 @@ var imgNumber = 1;
 //      })
 ////      .catch(error => console.error(error));
 //}
+//function caricaFotoDaFile_big(fileInput){
+//var imgNumber = 1;
+//    fetch(fileInput)
+//      .then(response => response.text())
+//      .then(data => {
+//        const lines = data.split('\n');
+//        const container = document.getElementById('galleriaBig');
+//        lines.forEach(line => {
+////        img grandi
+//          const div = document.createElement('div');
+//          div.classList.add("mySlides");
+//          div.id="placeHere";
+//          var elem = document.createElement("img");
+//          elem.setAttribute("src", line+1024);
+//          elem.setAttribute("style", "width:100%");
+//          container.appendChild(div);
+//          document.getElementById("placeHere").appendChild(elem);
+//          document.getElementById("placeHere").id="";
+//        });
+////        lines.forEach(line => {
+////        //          img piccole (miniature)
+////          const divM = document.createElement('div');
+////          divM.classList.add("column");
+////          divM.id="placeHere";
+////          var elem = document.createElement("img");
+////          elem.classList.add("demo");
+////          elem.classList.add("cursor");
+////          elem.setAttribute("src", line+256);
+////          elem.setAttribute("onclick","currentSlide("+imgNumber+")");
+////          elem.setAttribute("alt","Nature and sunrise");
+////          elem.setAttribute("style", "width:100%");
+////          container.appendChild(divM);
+////          document.getElementById("placeHere").appendChild(elem);
+////          document.getElementById("placeHere").id="";
+////          imgNumber=imgNumber+1;
+////      })
+//      });
+////      .catch(error => console.error(error));
+//}
+////----google Photo load
+//function listGoogleFolderFile(){
+//    const apiKey = "AIzaSyB9sIIxnF3LTS6-7oN-D3HbV4ZGOz5ReT4";
+//    const folderId = '1MRnPPdsmNHNHdtSG0sSpuKkNmdrmUQiC';
+////    https://drive.google.com/drive/folders/1MRnPPdsmNHNHdtSG0sSpuKkNmdrmUQiC?usp=sharing
+////GET https://www.googleapis.com/drive/v2/files?q=a1MRnPPdsmNHNHdtSG0sSpuKkNmdrmUQiC&key=AIzaSyB9sIIxnF3LTS6-7oN-D3HbV4ZGOz5ReT4
+//    const apiUrl = `https://www.googleapis.com/drive/v3/files?q='${folderId}' in parents&key=${apiKey}`;
+//    console.log(apiUrl);
+////    console.log(apiUrl);
+//    fetch(apiUrl)
+//    .then(response => response.json())
+//    .then(data => {
+//        const files = data.files;
+//        if (files && files.length > 0) {
+//          files.forEach(file => {
+//            console.log(file.name, '(', file.id, ')');
+//          });
+//        } else {
+//          console.log('No files found.');
+//        }
+//        })
+//        .catch(error => {
+//        console.error('Error:', error);
+//    });
+//}
+//function caricaFotoDaGoogle_small(folderId){
+//var imgNumber = 1;
+//    const apiKey = "AIzaSyB9sIIxnF3LTS6-7oN-D3HbV4ZGOz5ReT4";
+//    const apiUrl = `https://www.googleapis.com/drive/v3/files?q='${folderId}' in parents&key=${apiKey}`;
+//    console.log(apiUrl);
+//    fetch(apiUrl)
+//    .then(response => response.json())
+//    .then(data => {
+//        const container = document.getElementById('galleriaSmall');
+//        const containerBig = document.getElementById('galleriaBig');
+//        const files = data.files;
+//        if (files && files.length > 0) {
+//            files.forEach(file => {
+////            costruisco le img piccole
+//                const div = document.createElement('div');
+//                div.classList.add("galleriaScacchiera_photoDiv");
+//                div.id="placeHere";
+//                var elem = document.createElement("img");
+////                https://www.googleapis.com/drive/v3/files/1c9biQJcw4hbG19iV318EPLet53LPSMSt?alt=media&key=AIzaSyB9sIIxnF3LTS6-7oN-D3HbV4ZGOz5ReT4
+//                elem.setAttribute("src", "https://www.googleapis.com/drive/v3/files/"+file.id+'?alt=media&key='+apiKey);
+//
+////                elem.setAttribute("src", "https://drive.google.com/uc?export=view&id="+file.id);
+//                elem.setAttribute("alt","Immagine da Google Drive");
+////                                elem.setAttribute("src", "https://drive.usercontent.google.com/download?export=view&authuser=0&id=1hEBbe4Jb6MbaDokcmCLorSpwvh66Xen3");
+//
+//                elem.setAttribute("style", "width:100%; max-height: 300px; ");
+//                elem.setAttribute("onclick", "openModal();currentSlide("+imgNumber+")");
+//                elem.setAttribute("class", "hover-shadow cursor")
+//                container.appendChild(div);
+//                document.getElementById("placeHere").appendChild(elem);
+//                document.getElementById("placeHere").id="";
+//                imgNumber=imgNumber+1;
+// //            costruisco le img grandi
+//                const divBig = document.createElement('div');
+//                divBig.classList.add("mySlides");
+//                divBig.id="placeHere";
+//                var elemBig = document.createElement("img");
+//                 elemBig.setAttribute("src", "https://www.googleapis.com/drive/v3/files/"+file.id+'?alt=media&key='+apiKey);
+////                elemBig.setAttribute("src", "https://drive.google.com/uc?export=view&id="+file.id);
+//                elemBig.setAttribute("alt","Immagine da Google Drive");
+////                                elemBig.setAttribute("src", "https://drive.usercontent.google.com/download?export=view&authuser=0&id=1hEBbe4Jb6MbaDokcmCLorSpwvh66Xen3");
+//                elemBig.setAttribute("style", "width:100%;");
+//                containerBig.appendChild(divBig);
+//                document.getElementById("placeHere").appendChild(elemBig);
+//                document.getElementById("placeHere").id="";
+//            });
+//        }
+//    });
+//}
+//
+////    var imgNumber = 1;
+////    fetch(fileInput)
+////      .then(response => response.text())
+////      .then(data => {
+////        const lines = data.split('\n');
+////        const container = document.getElementById('galleriaSmall');
+////        lines.forEach(line => {
+////          const div = document.createElement('div');
+////          div.classList.add("galleriaScacchiera_photoDiv");
+////          div.id="placeHere";
+////          var elem = document.createElement("img");
+////          elem.setAttribute("src", line+256);
+////          elem.setAttribute("style", "width:100%");
+////          elem.setAttribute("onclick", "openModal();currentSlide("+imgNumber+")");
+////          elem.setAttribute("class", "hover-shadow cursor")
+////          container.appendChild(div);
+////          document.getElementById("placeHere").appendChild(elem);
+////          document.getElementById("placeHere").id="";
+////          imgNumber=imgNumber+1;
+////        });
+////      })
+//////      .catch(error => console.error(error));
+////}
